9b100adbafc1d1626300c107edd5ec80ccb51c72
techvna-coder <vnarwd@gmail.com>
2025-10-03 10:43:32 +0700

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,19 @@
+"""
+Core package for ATA WO Analyzer
+"""
+from .wo_processor import WOProcessor, WOResult
+from .non_defect_filter import NonDefectFilter
+from .citation_extractor import CitationExtractor
+from .decision_engine import DecisionEngine
+from .ata_catalog import ATACatalog
+
+__all__ = [
+    'WOProcessor',
+    'WOResult',
+    'NonDefectFilter',
+    'CitationExtractor',
+    'DecisionEngine',
+    'ATACatalog',
+]
+
+__version__ = '1.0.0'
